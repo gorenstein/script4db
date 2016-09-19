63b01c37d539cd3e6808e92cef28ab21be0b862e
serge <digitkraft@gmail.com>
2016-09-20 00:12:34 +0200

    Using window forms vs WPF.  Add icon & About dialog.

---
+++ script4db/script4db/FormMain.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace script4db
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.buttonPauseContinue = new System.Windows.Forms.Button();
+            this.buttonBreak = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.textBoxScriptFile = new System.Windows.Forms.TextBox();
+            this.buttonAbout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOpen.Location = new System.Drawing.Point(502, 12);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(96, 31);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRun.Enabled = false;
+            this.buttonRun.Location = new System.Drawing.Point(502, 62);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(96, 31);
+            this.buttonRun.TabIndex = 1;
+            this.buttonRun.Text = "Run";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // buttonPauseContinue
+            // 
+            this.buttonPauseContinue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPauseContinue.Enabled = false;
+            this.buttonPauseContinue.Location = new System.Drawing.Point(502, 99);
+            this.buttonPauseContinue.Name = "buttonPauseContinue";
+            this.buttonPauseContinue.Size = new System.Drawing.Size(96, 31);
+            this.buttonPauseContinue.TabIndex = 2;
+            this.buttonPauseContinue.Text = "Pause";
+            this.buttonPauseContinue.UseVisualStyleBackColor = true;
+            this.buttonPauseContinue.Click += new System.EventHandler(this.buttonPauseContinue_Click);
+            // 
+            // buttonBreak
+            // 
+            this.buttonBreak.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonBreak.Enabled = false;
+            this.buttonBreak.Location = new System.Drawing.Point(502, 147);
+            this.buttonBreak.Name = "buttonBreak";
+            this.buttonBreak.Size = new System.Drawing.Size(96, 31);
+            this.buttonBreak.TabIndex = 3;
+            this.buttonBreak.Text = "Break";
+            this.buttonBreak.UseVisualStyleBackColor = true;
+            this.buttonBreak.Click += new System.EventHandler(this.buttonBreak_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExit.Location = new System.Drawing.Point(502, 328);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(96, 31);
+            this.buttonExit.TabIndex = 4;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // textBoxScriptFile
+            // 
+            this.textBoxScriptFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxScriptFile.Location = new System.Drawing.Point(12, 15);
+            this.textBoxScriptFile.Name = "textBoxScriptFile";
+            this.textBoxScriptFile.ReadOnly = true;
+            this.textBoxScriptFile.Size = new System.Drawing.Size(484, 26);
+            this.textBoxScriptFile.TabIndex = 5;
+            // 
+            // buttonAbout
+            // 
+            this.buttonAbout.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAbout.Location = new System.Drawing.Point(502, 291);
+            this.buttonAbout.Name = "buttonAbout";
+            this.buttonAbout.Size = new System.Drawing.Size(96, 31);
+            this.buttonAbout.TabIndex = 6;
+            this.buttonAbout.Text = "About";
+            this.buttonAbout.UseVisualStyleBackColor = true;
+            this.buttonAbout.Click += new System.EventHandler(this.buttonAbout_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 372);
+            this.Controls.Add(this.buttonAbout);
+            this.Controls.Add(this.textBoxScriptFile);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonBreak);
+            this.Controls.Add(this.buttonPauseContinue);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.buttonOpen);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "script4db";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Button buttonPauseContinue;
+        private System.Windows.Forms.Button buttonBreak;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.TextBox textBoxScriptFile;
+        private System.Windows.Forms.Button buttonAbout;
+    }
+}
+
---
+++ script4db/script4db/FormMain.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace script4db
+{
+    public partial class FormMain : Form
+    {
+        enum Status
+        {
+            Init,
+            Parse,
+            Run,
+            Continue,
+            Finish,
+            Pause,
+            Break,
+            Error
+        }
+
+        Status CurrentStatus;
+        String InitialDirectory = Environment.SpecialFolder.Desktop.ToString();
+        //String InitialDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            this.MinimumSize = new Size(this.Width, this.Height);
+            refreshControls(Status.Init);
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonAbout_Click(object sender, EventArgs e)
+        {
+            AboutBox aboutBox = new AboutBox();
+            aboutBox.ShowDialog();
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            // Displays an OpenFileDialog so the user can select a Cursor.
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Script Files (*.script4db)|*.script4db";
+            openFileDialog.Title = "Select a Script File";
+            openFileDialog.InitialDirectory = InitialDirectory;
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.CheckPathExists = true;
+
+            // Show the Dialog.
+            // If the user clicked OK in the dialog and
+            // a script file was selected, open it.
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxScriptFile.Text = openFileDialog.FileName;
+                InitialDirectory = Path.GetDirectoryName(openFileDialog.FileName);
+                refreshControls(Status.Parse);
+                refreshControls(Status.Run);
+            }
+            else
+            {
+                textBoxScriptFile.Text = "";
+                refreshControls(Status.Init);
+            }
+        }
+
+        private void refreshControls(Status newStatus)
+        {
+
+            CurrentStatus = newStatus;
+
+            buttonOpen.Enabled = false;
+            buttonRun.Enabled = false;
+            buttonPauseContinue.Enabled = false;
+            buttonBreak.Enabled = false;
+            buttonExit.Enabled = false;
+
+            switch (CurrentStatus)
+            {
+                case Status.Init:
+                    buttonOpen.Enabled = true;
+                    buttonExit.Enabled = true;
+                    break;
+                case Status.Parse:
+                    //this.
+                    buttonExit.Enabled = true;
+                    break;
+                case Status.Break:
+                case Status.Error:
+                    buttonOpen.Enabled = true;
+                    buttonExit.Enabled = true;
+                    break;
+                case Status.Run:
+                    buttonPauseContinue.Enabled = true;
+                    buttonBreak.Enabled = true;
+                    break;
+                case Status.Finish:
+                    buttonOpen.Enabled = true;
+                    buttonExit.Enabled = true;
+                    break;
+                case Status.Pause:
+                case Status.Continue:
+                    buttonPauseContinue.Text = CurrentStatus.ToString();
+                    buttonPauseContinue.Enabled = true;
+                    buttonBreak.Enabled = true;
+                    break;
+                default:
+                    throw new System.ArgumentException("Default switch case must not be never reachable by refreshControls.", "appStatusError");
+            }
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            refreshControls(Status.Run);
+        }
+
+        private void buttonPauseContinue_Click(object sender, EventArgs e)
+        {
+            Status NewStatus;
+            if (CurrentStatus == Status.Run || CurrentStatus == Status.Pause)
+                NewStatus = Status.Continue;
+            else if (CurrentStatus == Status.Continue)
+                NewStatus = Status.Pause;
+            else
+                throw new System.ArgumentException("Error application status: " + CurrentStatus.ToString() + " - Must be Pause or Continue.", "appStatusError");
+
+            refreshControls(NewStatus);
+        }
+
+        private void buttonBreak_Click(object sender, EventArgs e)
+        {
+            refreshControls(Status.Break);
+        }
+    }
+}
